2986cc5fc35100bde0d1bdb8170333ae8537bd90
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2018-05-08 10:06:27 +0200

    initial commit

---
+++ 1.Kalkulator/Kalkulator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witamy na kursie z automatyzacji");
+        }
+    }
+}
