1e504f50c9e24e96c189a326697a359999d85ed0
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2018-05-08 10:30:48 +0200

    created calculator class with one operation

--- 1.Kalkulator/Kalkulator/Program.cs
+++ 1.Kalkulator/Kalkulator/Program.cs
@@ -11,6 +11,26 @@ namespace Kalkulator
         static void Main(string[] args)
         {
             Console.WriteLine("Witamy na kursie z automatyzacji");
+
+            var x = int.Parse(Console.ReadLine());
+            var y = int.Parse(Console.ReadLine());
+
+            var calc = new ExampleCalculator();
+            var result = calc.Add(x, y);
+
+            Console.WriteLine(result);
+        }
+    }
+
+    internal class ExampleCalculator
+    {
+        public ExampleCalculator()
+        {
+        }
+
+        internal int Add(int x, int y)
+        {
+            return x + y;
         }
     }
 }
