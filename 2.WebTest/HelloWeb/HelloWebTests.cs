8a5bff320834fddab6da778253f4baf911dc2506
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2018-05-08 16:06:15 +0200

    added first web test using selenium

---
+++ 2.WebTest/HelloWeb/HelloWebTests.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Xunit;
+
+namespace HelloWeb
+{
+    public class HelloWebTests : IDisposable
+    {
+        private readonly string BlogUrl = "https://autotestdotnet.wordpress.com/";
+        private IWebDriver browser;
+
+        public HelloWebTests()
+        {
+            browser = new ChromeDriver();
+        }
+
+        [Fact]
+        public void Can_open_blog_and_hello_note_exists()
+        {
+            browser.Navigate().GoToUrl(BlogUrl);
+
+            var article = browser.FindElement(By.Id("post-3096"));
+            var header = article.FindElement(By.TagName("h1"));
+
+            Assert.Equal("Witamy na warsztatach automatyzacji testów!", header.Text);
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                browser.Quit();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
