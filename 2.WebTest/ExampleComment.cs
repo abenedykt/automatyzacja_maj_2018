5e3ad74d169849d3da483fc70a959755bcb54a41
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2018-05-09 12:32:13 +0200

    added example using page object

---
+++ 2.WebTest/ExampleComment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HelloWeb
+{
+    internal class ExampleComment
+    {
+        public string Name { get; }
+        public string Email { get; }
+        public string Text { get; }
+
+        public ExampleComment()
+        {
+            Name = GenerateUserName();
+            Email = GenerateEmail();
+            Text = GenerateComment();
+        }
+
+        private string GenerateComment()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        private string GenerateUserName()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        private string GenerateEmail()
+        {
+            var user = Guid.NewGuid().ToString();
+            return $"{user}@nonexistent.test.com";
+        }
+    }
+}
---
+++ 2.WebTest/Note.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium;
+using Xunit;
+
+namespace HelloWeb
+{
+    internal class Note
+    {
+        private IWebDriver _driver;
+        private readonly string _url;
+
+        public Note(IWebDriver driver, string url)
+        {
+            _driver = driver;
+            _url = url;
+            _driver.Navigate().GoToUrl(url);
+        }
+
+        internal void AddComment(ExampleComment comment)
+        {
+            var commentElement = _driver.FindElement(By.Id("comment"));
+            commentElement.SendKeys(comment.Text);
+
+            var emailElement = _driver.FindElement(By.Id("email"));
+            emailElement.SendKeys(comment.Email);
+
+            var userElement = _driver.FindElement(By.Id("author"));
+            userElement.SendKeys(comment.Name);
+
+            var submitElement = _driver.FindElement(By.Id("comment-submit"));
+
+            submitElement.Click();
+        }
+
+        internal IEnumerable<IWebElement> SearchCommentsByText(ExampleComment comment)
+        {
+            var comments = _driver.FindElements(By.ClassName("comment-content"));
+
+            return comments.Where(c => c.Text.Contains(comment.Text));
+        }
+    }
+}
