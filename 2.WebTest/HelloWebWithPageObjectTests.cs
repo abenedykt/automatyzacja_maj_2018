1f0d9a38ddaab023d3b4e440ec3d2d0c81080570
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2018-05-09 13:45:29 +0200

    restoring example withoout autodata

--- 2.WebTest/HelloWebWithPageObjectTests.cs
+++ 2.WebTest/HelloWebWithPageObjectTests.cs
@@ -10,15 +10,31 @@ namespace HelloWeb
     {
         private const string FirstNoteUrl = "https://autotestdotnet.wordpress.com/2018/05/07/witamy-na-warsztatach-automatyzacji-testow";
         private IWebDriver driver;
+        private ExampleComment exampleComment;
 
         public HelloWebWithPageObjectTests()
         {
             driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
+            exampleComment = new ExampleComment();
+        }
+
+        [Fact]
+        public void Can_add_comment_to_existing_note()
+        {
+            //arrange
+            var welcomeNote = new Note(driver, FirstNoteUrl);
+
+            // act
+            welcomeNote.AddComment(exampleComment);
+
+            //assert
+            var comments = welcomeNote.SearchCommentsByText(exampleComment);
+            Assert.Single(comments);
         }
 
         [Theory, AutoData]
-        public void Can_add_comment_to_existing_note(ExampleComment comment)
+        public void Can_add_comment_to_existing_note_with_autodata(ExampleComment comment)
         {
             //arrange
             var welcomeNote = new Note(driver, FirstNoteUrl);
